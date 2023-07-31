b1d27e45d5030c78e74dc09c535e2cffc5595854
Marc Nicolas Oliva <nicolm7@mcmaster.ca>
2023-07-31 08:58:02 -0400

    Placeholder for command classes

---
+++ buycommand.py
@@ -0,0 +1,11 @@
+from command import Command
+
+
+class BuyStockCommand(Command):
+    def __init__(self, symbol, price, quantity):
+        self._symbol = symbol
+        self._price = price
+        self._quantity = quantity
+
+    def execute():
+        return 0
