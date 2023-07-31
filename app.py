36ca4d86a8b9c988acb671bfca2e7317b93e82b5
Marc Nicolas Oliva <nicolm7@mcmaster.ca>
2023-07-30 21:54:59 -0400

    select_option added

--- app.py
+++ app.py
@@ -21,19 +21,19 @@ def print_menu():
     screen.refresh()
     curses.napms(200)
     screen.addstr(6,3, "0. Exit")
-def select_option():
-    return 0
-def menu():
-    option = -1
-    print_menu()
+def select_option(option):
+    curses.noecho()
     while(option == -1):
-        curses.noecho()
         option = screen.getch()-48
         if option == 0:
             screen.addstr(6,2,"")
             screen.refresh()
-            return None
-
+    return option
+    
+def menu():
+    option = -1
+    print_menu()
+    option = select_option(option)
     curses.curs_set(1)
     screen.addstr(option+1,3,"")
     screen.refresh()
