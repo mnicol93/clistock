a618ae66e59ed9d356b131b551f60b3ff94cc849
Marc Nicolas Oliva <nicolm7@mcmaster.ca>
2023-07-31 22:32:50 -0400

    Comments deleted

--- app.py
+++ app.py
@@ -14,35 +14,6 @@ sdata = StockDataFetcher()
 total_investment = 0
 
 
-# def print_menu():
-#     curses.curs_set(0)
-#     screen.addstr(
-#         0, 2, "Welcome to CLIStock! What would you like to do? Please, introduce a number\n")
-#     screen.addstr(
-#         1, 2, "==========================================================================\n")
-
-#     screen.addstr(2, 3, "1. Check portfolio")
-#     screen.refresh()
-#     curses.napms(200)
-#     screen.addstr(3, 3, "2. Check individual stock price")
-#     screen.refresh()
-#     curses.napms(200)
-#     screen.addstr(4, 3, "3. Add stock to portfolio")
-#     screen.refresh()
-#     curses.napms(200)
-#     screen.addstr(5, 3, "4. Sell stock from portfolio")
-#     screen.refresh()
-#     curses.napms(200)
-#     screen.addstr(6, 3, "0. Exit")
-
-
-# def select_option(option):
-#     curses.noecho()
-#     while (option < 0 or option > 4):
-#         option = screen.getch()-48
-#     return option
-
-
 def menu():
     option = -1
     print_menu(screen, curses)
